420e0c3779f0a2d8fa7c5c6237b4e784c0011a0e
AbdElrahman_Aziz <89615122+Abdelrahmanzezo@users.noreply.github.com>
2022-09-28 18:42:58 +0200

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,10 @@
 from pytube import YouTube
 try:
+    # Entering the link of the video
     url = "https://www.youtube.com/watch?v=MbTM6xnl5Qc"
     streams = YouTube(url).streams.get_lowest_resolution()
+    print("wait some seconds ..")
+    # where you want to download.....! 
     streams.download(r"D:\snap Tube")
     print("Done....... ")
 except:
